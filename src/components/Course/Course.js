399013a4666714b8907a339ebdd9127814844aa9
ali <s183033@student.dtu.dk>
2022-03-06 23:49:07 +0100

    c

---
+++ src/components/Course/Course.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import"./ReachUs.css"
+import { Container } from "react-bootstrap";
+
+class Course extends Component {
+    render(){
+        return <div className="Coursediv">
+            <figure class='PR'>
+                <figcaption className="Course1"> 
+                <h2>Courses</h2>
+                <h3>All</h3>
+                <p>Advanced Programming </p>
+                </figcaption>
+            </figure>
+            </div>
+    }
+}
+
+
+export default Course;
