e4140f9a5c4edb8259f359533b604203e9191987
salim963 <70900247+salim963@users.noreply.github.com>
2022-03-08 11:08:32 +0100

    cleaned

--- src/App.js
+++ src/App.js
@@ -1,49 +1,14 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import FirstPic from './components/FirstPic/FirstPic';
-import OurMission from './components/OurMission/OurMission';
-import AboutUs from './components/AboutUs/AboutUs';
-import ReachUs from './components/ReachUs/ReachUs';
-import { Container } from 'react-bootstrap';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import FirstPic from "./components/FirstPic/FirstPic";
+import OurMission from "./components/OurMission/OurMission";
+import AboutUs from "./components/AboutUs/AboutUs";
+import ReachUs from "./components/ReachUs/ReachUs";
+import { Container } from "react-bootstrap";
 
 function App() {
-  return (
-<div className="App">
-
-    <div>
-      <div>
-      <Navbar/>
-      </div>
-    </div>
-
-    <div>
-      <div>
-      <FirstPic/>
-      </div>
-    </div>
-
-
-    <div>
-      <div> 
-      <OurMission/>
-      </div>
-    </div>
-
-    <div>
-      <div>
-      <AboutUs/>
-      </div>
-    </div>
-
-    <div>
-      <div>
-      <ReachUs/>
-      </div>
-    </div>
-
-</div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
