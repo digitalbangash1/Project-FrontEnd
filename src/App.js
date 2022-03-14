462649ef623f7fb954ca27866c926cb34217c600
digitalbangash1 <digitalbangash1@hotmail.com>
2022-03-14 22:03:25 +0100

    Sidebar2 created

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import { Route, Routes } from 'react-router-dom';
 import CourseList from './components/Courses/CourseList';
 import CourseDetails from './components/Courses/CourseDetails';
 import Home from './components/Home/Home';
+import Testing from './components/testing';
+import Description from './pages/description';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+          <Navbar />
+        
       <Routes>
         <Route path="courses" element={<CourseList />} />
+        <Route path="discription" element={<Description />} />
+        <Route path="testing" element={<Testing />} />
         <Route path="/courses/:courseId" element={<CourseDetails />} />
         <Route path="/" element={<Home />} />
       </Routes>
---
+++ src/components/testing.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Sidebar1 from "./Sidebar/Sidebar1";
+
+
+
+
+export default function Testing() {
+
+    
+    return (
+    
+    <div>
+
+<Sidebar1/>
+
+ 
+    </div>
+    )
+
+}
+
+
