10bdde90812d1fdcbd64f7f6f7311fd606082843
digitalbangash1 <digitalbangash1@hotmail.com>
2022-03-21 13:32:09 +0100

    course details

--- src/components/Courses/CourseDetails.js
+++ src/components/Courses/CourseDetails.js
@@ -9,6 +9,8 @@ export default function CourseDetails() {
         <div>
             
           <Sidebar/>
+          <div>Welcome to {course.name}</div>
+          <div>{course.description}</div>
         </div>
     );
 }
