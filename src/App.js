47ea40b34d86298e6d3c26b3216d884f055b2af5
salim963 <70900247+salim963@users.noreply.github.com>
2022-03-22 14:30:18 +0100

    login routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import Navbar from './components/Navbar/Navbar';
-import { Route, Routes } from 'react-router-dom';
-import CourseList from './components/Courses/CourseList';
-import CourseDetails from './components/Courses/CourseDetails';
-import Home from './components/Home/Home';
-import Testing from './components/testing';
-import Description from './pages/description';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import CourseList from "./components/Courses/CourseList";
+import CourseDetails from "./components/Courses/CourseDetails";
+import Home from "./components/Home/Home";
+import Testing from "./components/testing";
+import Login from "./components/login/login";
+
+
 
 
 function App() {
   return (
     <div className="App">
-          <Navbar />
-        
+      <Navbar />
+
       <Routes>
         <Route path="/courses" element={<CourseList />} />
         <Route path="/courses/:courseId" element={<CourseDetails />} />
         <Route path="testing" element={<Testing />} />
+        <Route path = "/login" element ={<Login/>} />
         <Route path="/" element={<Home />} />
       </Routes>
     </div>
---
+++ src/components/login/login.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+render() {
+    return <div className="test"> 
+        <h1>
+        hello salam salam 
+            </h1>
+    </div>;
+}
+}
+
+export default Login 
