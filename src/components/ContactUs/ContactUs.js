a78c4982ae4ca7287e29ad548b842920518a9e01
Strongside-87 <thomas.arildtoft@icloud.com>
2022-03-27 22:11:46 +0200

    commit

---
+++ src/components/ContactUs/ContactUs.js
@@ -0,0 +1,36 @@
+import emailjs from "emailjs-com";
+import React, { Component } from "react";
+
+export default function ContactUs() {
+
+  function sendEmail(e) {
+
+  }
+
+  return (
+    <div>
+      <div className="container">
+        <form onSubmit={sendEmail}>
+          <div className="row pt-5 mx-auto">
+            <div className="col-8 form-group mx-auto">
+              <input type="text" className="form-control" placeholder="name" name="name" />
+            </div>
+            <div className="col-8 form-group pt-2 mx-auto">
+              <input type="email" className="form-control" placeholder="Email Address" name="email" />
+            </div>
+            <div className="col-8 form-group pt-2 mx-auto">
+              <input type="text" className="form-control" placeholder="Subject" name="subject" />
+            </div>
+            <div className="col-8 form-group mx-auto">
+              <textarea className="form-control" id="" cols="30" row="8" placeholder="Your message" name="message"></textarea>
+            </div>
+            <div className="col-8 pt-3 mx-auto">
+              <input type="submit" className="btn btn-info" value="Send Message"></input>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
