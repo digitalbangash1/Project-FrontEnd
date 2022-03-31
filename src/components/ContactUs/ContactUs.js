993c2a77104b370df3d13c06a176b0a22240a302
Strongside-87 <thomas.arildtoft@icloud.com>
2022-03-31 11:40:59 +0200

    Css update

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,6 +1,7 @@
-
+import './ContactUs.css'
 import React, { Component } from "react";
 import emailjs from "emailjs-com";
+
 
 export default function ContactUs() {
 
@@ -18,29 +19,29 @@ export default function ContactUs() {
       e.target.reset();
   }
 
-  return (
-    <div>
+  return ( <div classname= "ContactUs" id="ContactForm">
       <div>
-        <h3 id="header">Do you have any suggestions?</h3>
+        <h3 id="text">Do you have any suggestions?</h3>
         <div id="text">We are ready to work on your suggestions of any complexity, whether it’s about the courses stated above or you want us to list new courses.</div>
       </div>
       <div className="container">
         <form onSubmit={sendEmail}>
           <div className="row pt-5 mx-auto">
             <div className="col-8 form-group mx-auto">
-              <input type="text" className="form-control" placeholder="Name" name="name" />
+              <input type="text" className="form-control" placeholder="Your Name*" name="name" />
             </div>
             <div className="col-8 form-group pt-2 mx-auto">
-              <input type="email" className="form-control" placeholder="Email Address" name="email" />
+              <input type="email" className="form-control" placeholder="Email Address*" name="email" />
             </div>
             <div className="col-8 form-group pt-2 mx-auto">
-              <input type="text" className="form-control" placeholder="Subject" name="subject" />
+              <input type="text" className="form-control" placeholder="Subject*" name="subject" />
             </div>
             <div className="col-8 form-group mx-auto">
               <textarea className="form-control" id="" cols="30" row="8" placeholder="Your message" name="message"></textarea>
             </div>
+            <div id="text"><mark class="red">*</mark> indicates a required field</div>
             <div className="col-8 pt-3 mx-auto">
-              <input type="submit" className="btn btn-info" value="Send Message"></input>
+              <input type="submit" className="btn btn-info" id="MsgBtn" value="Send Message"></input>
             </div>
           </div>
         </form>
