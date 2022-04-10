850aa1279df35c8d0a6ba540b9afad89c6e62e26
ali <s183033@student.dtu.dk>
2022-04-11 00:23:49 +0200

    course slider

---
+++ src/components/Slider/sliderImage.js
@@ -0,0 +1,22 @@
+import Dsystems from "./Dsystems.jpg";
+import frontend from "./frontend.png";
+import backend from "./backend.jpg";
+
+export default [
+  {
+    
+    description: "This is the first slider Image of our carousel",
+    urls: Dsystems,
+  },
+  {
+    
+    description: "This is the second slider Image of our carousel",
+    urls: frontend,
+  },
+  {
+    
+    description: "This is the Third slider Image of our carousel",
+    urls: backend,
+  },
+  
+];
