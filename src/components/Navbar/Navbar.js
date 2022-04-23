4b36f7705e425c36852afa50521cf890088d026f
Strongside-87 <thomas.arildtoft@icloud.com>
2022-04-23 21:31:27 +0200

    Navbar update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,13 +11,7 @@ class Navbar extends Component {
         return (
 
             <nav className="NavbarItems">
-                <h1 className='navbar-logo'>Engineer Academy <i className="fab fa-react"></i></h1>
-                <div className='menu-icon' onClick={this.handleClick}>
-                    <i className={this.state.clicked ? 'fas fa times' : 'fas fa-bars'}></i>
-
-                </div>
-
-                
+            
                 <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
                     {MenuItems.map((item, index) => {
                         return (
