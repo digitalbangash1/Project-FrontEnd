25496fdfcfc595859cb1f8fabde2fc98a4eba4cd
ali <s183033@student.dtu.dk>
2022-04-27 13:23:35 +0200

    css change

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -6,16 +6,16 @@ import sliderImage from "./sliderImage";
 import "./slider.css";
 
 const len = sliderImage.length - 1;
-
 function Slider(_props) {
+  
   const [activeIndex, setActiveIndex] = useState(0);
-
   useEffect(() => {
     const interval = setInterval(() => {
       setActiveIndex(activeIndex === len ? 0 : activeIndex + 1);
     }, 5000);
     return () => clearInterval(interval);
   }, [activeIndex]);
+  
 
   return (
     <div className="slider-container">
