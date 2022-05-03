e10ebaa051ed15f5e90356ab538b753fb0e22323
Muhammad Bangash <s092512@student.dtu.dk>
2022-05-03 17:20:31 +0200

    referencing working

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+
 import { MenuItems } from "./MenuItems"
-import './Navbar.css'
+import './Navbar.css'   
 import logo from './Logo.png';
+
 
 class Navbar extends Component {
     state = { clicked: false }
@@ -14,12 +15,17 @@ class Navbar extends Component {
             <nav className="NavbarItems">
             <img className="NavLogo" src={logo}></img>
                 <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                    
                     {MenuItems.map((item, index) => {
                         return (
                             <li key={index}>
                                 <a className={item.cName} href = {item.url}>{item.title}</a>
+                                
                             </li>)
+
+                            
                     })}
+                    
 
                 </ul>
             </nav>
