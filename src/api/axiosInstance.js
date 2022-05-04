05faa0ce0fab50709a7d6ff10878a43e8091073f
Muhammad Bangash <s092512@student.dtu.dk>
2022-05-04 22:44:40 +0200

    add static web app routing

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://localhost:7109',
+    //baseURL: 'https://localhost:7109',
+    baseURL: 'http://engineersacadamy-api.azurewebsites.net',
     timeout: 10000
   });
