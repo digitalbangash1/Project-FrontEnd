f5a4d4ed47c80f47b09c8d85b2261e4873321bcb
salim963 <salimdk97@gmail.com>
2022-05-18 11:47:13 +0200

    Error 404,page not found

---
+++ src/components/NoPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+class NoPage extends Component {
+    render() {
+        return (
+            <div >
+                <h1>404</h1>
+                <p>
+                Error 404 page not found 
+                </p>
+            </div>
+        );
+    }
+}
+
+
+export default NoPage;
